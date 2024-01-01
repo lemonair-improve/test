7a837cb282dcffc23568cf3b17dddc54db56be14
kangminbeom <alsqja6945@naver.com>
2024-01-01 14:16:04 +0900

    script

--- scripts/minbeom/vu2000-maintain.js
+++ scripts/minbeom/vu2000-maintain.js
@@ -1,5 +1,5 @@
 import ws from "k6/ws";
-import { check } from "k6";
+import { check, sleep } from "k6";
 import { Counter } from "k6/metrics";
 
 const ip = "192.168.56.1";
@@ -7,12 +7,12 @@ const port = "8082";
 const chatRoomName = "testRoom";
 const messageReceiveCounter = new Counter("message_receive_counter");
 
-const userCount = 2000;
-
 export const options = {
-  vus: userCount,
-  duration: "50s",
-  iterations: userCount,
+  stages: [
+    { duration: '10m', target: 2000 },
+    { duration: '20m', target: 2000 },
+    { duration: '10m', target: 0 },
+  ],
 };
 
 export default function () {
@@ -21,6 +21,7 @@ export default function () {
   const res = ws.connect(url, params, function (socket) {
     socket.on("open", () => {
       console.log(`VU ${__VU} connected`);
+      sendChat(socket); // Open 시에 바로 채팅을 보내도록 호출
     });
 
     socket.on("message", (data) => {
@@ -31,4 +32,12 @@ export default function () {
     socket.on("close", () => console.log(`User ${__VU} disconnected`));
   });
   check(res, { "status is 101": (r) => r && r.status === 101 });
+}
+
+function sendChat(socket) {
+  const interval = 100;
+  for (let i = 0; i < chatMessagesToSend; i++) {
+    socket.send(JSON.stringify({ message: `Chat message ${i + 1}` }));
+    sleep(interval);
+  }
 }
